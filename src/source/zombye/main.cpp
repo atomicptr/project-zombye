568467be5e8f42d80d1993c945c0bc417ee1e044
Christopher Kaster <ikasoki@gmail.com>
2014-10-23 23:43:33 +0200

    add os info to main

--- src/source/zombye/main.cpp
+++ src/source/zombye/main.cpp
@@ -2,9 +2,10 @@
 #include <string>
 
 #include <zombye/core/game.hpp>
+#include <zombye/utils/os.h>
 
 int main() {
-    std::cout << "init zombye" << std::endl;
+    std::cout << "init zombye with OS: " << OS_NAME << std::endl;
 
     zombye::game game("project-zombye", 800, 600);
 
