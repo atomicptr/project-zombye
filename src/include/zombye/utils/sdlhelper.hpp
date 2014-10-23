d2160af8a7207f9162e131e28fade3976ebd29c3
Christopher Kaster <ikasoki@gmail.com>
2014-10-23 18:13:01 +0200

    add sdlhelper with make_window

---
+++ src/include/zombye/utils/sdlhelper.hpp
@@ -0,0 +1,19 @@
+#ifndef __ZOMBEYE_SDLHELPER_HPP__
+#define __ZOMBEYE_SDLHELPER_HPP__
+
+#include <utility>
+
+#include <SDL2/SDL.h>
+
+#include <zombye/utils/make_smart.hpp>
+
+namespace zombeye {
+
+    template<typename... Arguments>
+    auto make_window(Arguments... args) {
+        return make_smart<SDL_Window>(SDL_CreateWindow,
+            SDL_DestroyWindow, std::forward<Arguments>(args)...);
+    }
+}
+
+#endif
