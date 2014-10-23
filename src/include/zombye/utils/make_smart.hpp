5a754482f7d11333e896f6ba8132eda7c21c5229
Christopher Kaster <ikasoki@gmail.com>
2014-10-23 18:06:26 +0200

    add make_smart.hpp

---
+++ src/include/zombye/utils/make_smart.hpp
@@ -0,0 +1,22 @@
+#ifndef __ZOMBYE_SMART_HPP__
+#define __ZOMBYE_SMART_HPP__
+
+#include <memory>
+#include <stdexcept>
+#include <utility>
+
+namespace zombeye {
+
+    template<typename T, typename Constructor, typename Destructor, typename... Arguments>
+    auto make_smart(Constructor construct, Destructor destroy, Arguments... args) {
+        auto ptr = construct(std::forward<Arguments>(args)...);
+
+        if(!ptr) {
+            throw std::runtime_error("Err: not able to create smart pointer");
+        }
+
+        return std::unique_ptr<T, void(*)(T*)>(ptr, destroy);
+    }
+}
+
+#endif
