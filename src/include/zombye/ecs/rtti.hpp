7f427f95a94b7cdda71ede748ba260214ed502bc
Berling <georg.peter.schaefer@googlemail.com>
2014-10-27 21:37:40 +0100

    add vector of properties to rtti class that are registrated by the owning class

--- src/include/zombye/ecs/rtti.hpp
+++ src/include/zombye/ecs/rtti.hpp
@@ -5,17 +5,35 @@
 #include <string>
 
 namespace zombye {
+    class abstract_property;
     class component;
     class entity;
     class game;
-    struct rtti {
-        unsigned long type_id;
-        std::string type_name;
-        std::shared_ptr<rtti> base_rtti;
-        std::function<component*(game&, entity&)> factory;
+    class rtti {
+        unsigned long type_id_;
+        std::string type_name_;
+        std::shared_ptr<rtti> base_rtti_;
+        std::function<component*(game&, entity&)> factory_;
+        std::vector<std::shared_ptr<abstract_property>> properties_;
+    public:
         rtti(unsigned long type_id, const std::string& type_name, const std::shared_ptr<rtti>& base_rtti,
             std::function<component*(game&, entity&)> factory) noexcept
-        : type_id(type_id), type_name(type_name), base_rtti(base_rtti), factory(factory) { }
+        : type_id_(type_id), type_name_(type_name), base_rtti_(base_rtti), factory_(factory) { }
+        unsigned long type_id() const noexcept {
+            return type_id_;
+        }
+        const std::string& type_name() const noexcept {
+            return type_name_;
+        }
+        std::shared_ptr<const rtti> base_rtti() const noexcept {
+            return base_rtti_;
+        }
+        const std::function<component*(game&, entity&)>& factory() const noexcept {
+            return factory_;
+        }
+        const std::vector<std::shared_ptr<abstract_property>>& properties() const noexcept {
+            return properties_;
+        }
     };
 }
 
