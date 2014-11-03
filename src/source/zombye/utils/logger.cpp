6b772af5e24041533eed95b2a05c28c4a42395f5
Christopher Kaster <ikasoki@gmail.com>
2014-11-03 15:15:57 +0100

    add missing [FATAL] output

--- src/source/zombye/utils/logger.cpp
+++ src/source/zombye/utils/logger.cpp
@@ -46,6 +46,9 @@ void zombye::log(log_level level, std::string msg) {
         case LOG_ERROR:
             ss << "[ERROR]";
             break;
+        case LOG_FATAL:
+            ss << "[FATAL]";
+            break;
     }
 
     ss << " " << msg << std::endl;
