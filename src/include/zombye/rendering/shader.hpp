d07d80823c62443432c0d4c8bdb108ac37f96910
Berling <georg.peter.schaefer@googlemail.com>
2014-11-10 14:57:45 +0100

    replace some variable declarations with auto

--- src/include/zombye/rendering/shader.hpp
+++ src/include/zombye/rendering/shader.hpp
@@ -7,14 +7,13 @@
 
 namespace zombye {
     class shader {
+        friend class shader_program;
         GLuint name_;
     public:
         shader(const std::string& file_name, GLenum type, const std::string& source);
         shader(const shader& other) = delete;
         shader(shader&& other) = delete;
         ~shader() noexcept;
-
-        GLuint name() const noexcept;
 
         shader& operator=(const shader& other) = delete;
         shader& operator=(shader&& other) = delete;
--- src/source/zombye/rendering/shader.cpp
+++ src/source/zombye/rendering/shader.cpp
@@ -8,24 +8,24 @@ namespace zombye {
     shader::shader(const std::string& file_name, GLenum type, const std::string& source)
     : name_{0} {
         glCreateShader(type);
-        const char* tmp = source.c_str();
+        auto tmp = source.c_str();
         glShaderSource(name_, 1, &tmp, nullptr);
         glCompileShader(name_);
-        int32_t length = 0;
+        auto length = int32_t{0};
         glGetShaderiv(name_, GL_INFO_LOG_LENGTH, &length);
-        std::vector<char> log_buffer;
+        auto log_buffer = std::vector<char>{};
         if (length > 1) {
             log_buffer.resize(length);
             glGetShaderInfoLog(name_, length, nullptr, log_buffer.data());
             log(LOG_ERROR, "compilation log of " + file_name + ": "
-                + std::string(log_buffer.begin(), log_buffer.end()));
+                + std::string{log_buffer.begin(), log_buffer.end()});
         }
-        int32_t status;
+        auto status = int32_t{0};
         glGetShaderiv(name_, GL_COMPILE_STATUS, &status);
         if (status == GL_FALSE) {
             glDeleteShader(name_);
             throw std::runtime_error("an error occured during compilation of " + file_name
-                + std::string(log_buffer.begin(), log_buffer.end()));
+                + std::string{log_buffer.begin(), log_buffer.end()});
         }
     }
 
