7181f56dd8446251c9d8fc69725c41602119bef5
Christopher Kaster <ikasoki@gmail.com>
2014-11-18 18:07:14 +0100

    Revert "remove georgs fancy camera and mesh stuff"
    This reverts commit 29c1a554537053a40cc66e712d5ec1ab23bcba4f.

--- src/source/zombye/gameplay/states/menu_state.cpp
+++ src/source/zombye/gameplay/states/menu_state.cpp
@@ -9,6 +9,10 @@ zombye::menu_state::menu_state(zombye::state_machine *sm) : sm_(sm) {
 
 void zombye::menu_state::enter() {
     zombye::log("enter menu state");
+    auto& camera = sm_->get_game()->entity_manager().emplace(glm::vec3{-2.f, 2.f, -3.0f}, glm::quat{}, glm::vec3{});
+    camera.emplace<camera_component>(glm::vec3{}, glm::vec3{0.f, 1.f, 0.f});
+    sm_->get_game()->rendering_system().activate_camera(camera.id());
+    sm_->get_game()->entity_manager().emplace("dummy", glm::vec3{}, glm::quat{}, glm::vec3{1});
 }
 
 void zombye::menu_state::leave() {
