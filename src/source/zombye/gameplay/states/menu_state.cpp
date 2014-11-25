15f34b304456f361a90d9ed01dd32d128111f837
Christopher Kaster <ikasoki@gmail.com>
2014-11-25 17:55:07 +0100

    add 2nd dummy to test physics

--- src/source/zombye/gameplay/states/menu_state.cpp
+++ src/source/zombye/gameplay/states/menu_state.cpp
@@ -19,9 +19,11 @@ void zombye::menu_state::enter() {
     camera.emplace<camera_component>(glm::vec3{}, glm::vec3{0.f, 1.f, 0.f});
     sm_->get_game()->rendering_system().activate_camera(camera.id());
 
-    auto& dummy = sm_->get_game()->entity_manager().emplace("dummy", glm::vec3{}, glm::quat{0, 0, 0, 1}, glm::vec3{1});
+    auto& dummy = sm_->get_game()->entity_manager().emplace("dummy", glm::vec3{0, 20, 0}, glm::quat{0, 0, 0, 1}, glm::vec3{1});
+    auto& dummy2 = sm_->get_game()->entity_manager().emplace("dummy", glm::vec3{0, -1, 0}, glm::quat{0, 0, 0, 1}, glm::vec3{1});
 
     dummy.emplace<physics_component>(new btBoxShape(btVector3(1, 1, 1)));
+    dummy2.emplace<physics_component>(new btBoxShape(btVector3(1, 1, 1)), true);
 }
 
 void zombye::menu_state::leave() {
