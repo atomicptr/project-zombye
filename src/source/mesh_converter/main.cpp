d9c9be280a3d09be6ceca81eac23c3c7d057f3db
Berling <georg.peter.schaefer@googlemail.com>
2014-11-30 19:22:43 +0100

    add collision mesh

--- src/source/mesh_converter/main.cpp
+++ src/source/mesh_converter/main.cpp
@@ -1,13 +1,20 @@
 #include <stdexcept>
+#include <string>
 
 #include <mesh_converter/mesh_converter.hpp>
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
+    if (argc < 3) {
         throw std::invalid_argument("invalid argument");
     }
     devtools::mesh_converter mc(argv[1], argv[2]);
-    mc.parse();
-    mc.serialize();
+    bool collision_geometry = false;
+    if (argc >= 4){
+        if (std::string{argv[3]} == "-c") {
+            collision_geometry = true;
+        }
+    }
+    mc.parse(collision_geometry);
+    mc.serialize(collision_geometry);
     return 0;
 }
