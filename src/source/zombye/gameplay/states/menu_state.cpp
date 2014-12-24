006f221273422aea434fc06544a5827dba54258c
Berling <georg.peter.schaefer@googlemail.com>
2014-12-24 22:31:40 +0100

    add light test scene

--- src/source/zombye/gameplay/states/menu_state.cpp
+++ src/source/zombye/gameplay/states/menu_state.cpp
@@ -16,12 +16,14 @@ zombye::menu_state::menu_state(zombye::state_machine *sm) : sm_(sm) {
 
 void zombye::menu_state::enter() {
     zombye::log("enter menu state");
-    auto& camera = sm_->get_game()->entity_manager().emplace(glm::vec3{2.f, 2.f, 3.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
-    camera.emplace<camera_component>(glm::vec3{0.f}, glm::vec3{0.f, 1.f, 0.f});
+    auto& camera = sm_->get_game()->entity_manager().emplace(glm::vec3{2.f, 1.f, -13.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
+    camera.emplace<camera_component>(glm::vec3{4.f, 0.f, 0.f}, glm::vec3{0.f, 1.f, 0.f});
     sm_->get_game()->rendering_system().activate_camera(camera.id());
-    sm_->get_game()->entity_manager().emplace("dummy", glm::vec3{0.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
-    sm_->get_game()->entity_manager().emplace("light", glm::vec3{-2.f, 2.f, 2.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
-    sm_->get_game()->entity_manager().emplace("light", glm::vec3{2.f, 2.f, 2.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
+    sm_->get_game()->entity_manager().emplace("light_test", glm::vec3{0.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
+    sm_->get_game()->entity_manager().emplace("light", glm::vec3{0.f, 2.f, -6.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
+    sm_->get_game()->entity_manager().emplace("red_light", glm::vec3{4.f, 1.f, -5.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
+    sm_->get_game()->entity_manager().emplace("green_light", glm::vec3{4.f, 0.f, -8.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
+    sm_->get_game()->entity_manager().emplace("red_light", glm::vec3{4.f, 0.f, 8.f}, glm::normalize(glm::quat{}), glm::vec3{1.f});
 }
 
 void zombye::menu_state::leave() {
