ef6b7b1331728ad7464f516e0d0ac3c816681690
Georg Schäfer <georg.peter.schaefer@gmail.com>
2015-05-05 23:45:50 +0200

    rewrite texture class to support creating textures without loading a resource image

--- src/include/zombye/rendering/texture.hpp
+++ src/include/zombye/rendering/texture.hpp
@@ -6,26 +6,37 @@
 
 namespace zombye {
     class texture {
+    private:
         GLuint id_;
+        GLenum target_;
         size_t width_;
         size_t height_;
+
     public:
         texture(const gli::texture2D& texture) noexcept;
-        texture(const texture& other) = delete;
-        texture(texture&& other) noexcept;
-        ~texture() noexcept;
-        texture& operator=(const texture& other) = delete;
-        texture& operator=(texture&& other) noexcept;
+        texture(GLenum target, GLint internal_format, GLsizei width, GLsizei height, GLenum format, GLenum type, const GLvoid* data = nullptr) noexcept;
+        ~texture();
+
+        texture(const texture& rhs) = delete;
+        texture& operator=(const texture& rhs) = delete;
+
+        texture(texture&& rhs) noexcept;
+        texture& operator=(texture&& rhs) noexcept;
 
         void bind(uint32_t unit) const noexcept;
 
-        size_t width() {
+        size_t width() const noexcept {
             return width_;
         }
 
-        size_t height() {
+        size_t height() const noexcept {
             return height_;
         }
+
+    private:
+        void apply_settings() const noexcept;
+
+        friend class framebuffer;
     };
 }
 
--- src/source/zombye/rendering/texture.cpp
+++ src/source/zombye/rendering/texture.cpp
@@ -2,65 +2,74 @@
 
 namespace zombye {
     texture::texture(const gli::texture2D& texture) noexcept
-    : width_{texture.dimensions().x}, height_{texture.dimensions().y} {
+    : width_{texture.dimensions().x}, height_{texture.dimensions().y}, target_{GL_TEXTURE_2D} {
         glGenTextures(1, &id_);
-        glBindTexture(GL_TEXTURE_2D, id_);
-        // adapted from http://www.g-truc.net/project-0024.html
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, GLint(texture.levels() - 1));
+        glBindTexture(target_, id_);
+        apply_settings();
+        // adapted from http://gli.g-truc.net/0.5.1/code.html
         if (gli::is_compressed(texture.format())) {
             for (gli::texture2D::size_type level = 0; level < texture.levels(); ++level) {
-                glCompressedTexImage2D(GL_TEXTURE_2D,
-                GLint(level),
-                GLenum{gli::internal_format(texture.format())},
-                GLsizei(texture[level].dimensions().x),
-                GLsizei(texture[level].dimensions().y),
+                glCompressedTexImage2D(target_,
+                static_cast<GLint>(level),
+                static_cast<GLenum>(gli::internal_format(texture.format())),
+                static_cast<GLsizei>(texture[level].dimensions().x),
+                static_cast<GLsizei>(texture[level].dimensions().y),
                 0,
-                GLsizei(texture[level].size()),
+                static_cast<GLsizei>(texture[level].size()),
                 texture[level].data());
             }
         } else {
             for (gli::texture2D::size_type level = 0; level < texture.levels(); ++level) {
-                glTexImage2D(GL_TEXTURE_2D,
-                GLint(level),
-                GLenum(gli::internal_format(texture.format())),
-                GLsizei(texture[level].dimensions().x),
-                GLsizei(texture[level].dimensions().y),
+                glTexImage2D(target_,
+                static_cast<GLint>(level),
+                static_cast<GLenum>(gli::internal_format(texture.format())),
+                static_cast<GLsizei>(texture[level].dimensions().x),
+                static_cast<GLsizei>(texture[level].dimensions().y),
                 0,
-                GLenum(gli::external_format(texture.format())),
-                GLenum(gli::type_format(texture.format())),
+                static_cast<GLenum>(gli::external_format(texture.format())),
+                static_cast<GLenum>(gli::type_format(texture.format())),
                 texture[level].data());
             }
         }
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     }
 
-    texture::texture(texture&& other) noexcept
-    : id_{other.id_}, width_{other.width_}, height_{other.height_} {
-        other.id_ = 0;
-        other.width_ = 0;
-        other.height_ = 0;
+    texture::texture(GLenum target, GLint internal_format, GLsizei width, GLsizei height, GLenum format, GLenum type, const GLvoid* data) noexcept
+    : width_{static_cast<size_t>(width)}, height_{static_cast<size_t>(height)}, target_{target} {
+        glGenTextures(1, &id_);
+        glBindTexture(target_, id_);
+        glTexImage2D(target_, 0, internal_format, width_, height_, 0, format, type, data);
+        glTexParameteri(target_, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(target_, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     }
 
-    texture::~texture() noexcept {
+    texture::~texture() {
         glDeleteTextures(1, &id_);
     }
 
-    texture& texture::operator=(texture&& other) noexcept {
-        id_ = other.id_;
-        width_ = other.width_;
-        height_ = other.height_;
-        other.id_ = 0;
-        other.width_ = 0;
-        other.height_ = 0;
+    texture::texture(texture&& rhs) noexcept
+    : id_{rhs.id_}, target_{rhs.target_}, width_{rhs.width_}, height_{rhs.height_} {
+        rhs.id_ = 0;
+    }
 
+    texture& texture::operator=(texture&& rhs) noexcept {
+        id_ = rhs.id_;
+        target_ = rhs.target_;
+        width_ = rhs.width_;
+        height_ = rhs.height_;
+        rhs.id_ = 0;
         return *this;
     }
 
     void texture::bind(uint32_t unit) const noexcept {
         glActiveTexture(GL_TEXTURE0 + unit);
-        glBindTexture(GL_TEXTURE_2D, id_);
+        glBindTexture(target_, id_);
+    }
+
+    void texture::apply_settings() const noexcept {
+        glTexParameteri(target_, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(target_, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        auto max_aniso = 0.f;
+        glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &max_aniso);
+        glTexParameteri(target_, GL_TEXTURE_MAX_ANISOTROPY_EXT, max_aniso);
     }
 }
