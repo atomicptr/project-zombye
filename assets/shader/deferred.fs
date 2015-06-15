4bd572e992f493059da7a28d658245e1b1c617f3
Georg Schäfer <georg.peter.schaefer@gmail.com>
2015-06-15 11:24:49 +0200

    fix shadow acne

--- assets/shader/deferred.fs
+++ assets/shader/deferred.fs
@@ -36,14 +36,18 @@ float sample_shadow(sampler2D shadow_map, vec2 texcoord, float compare) {
 	return step(compare, texture2D(shadow_map, texcoord).r);
 }
 
+float linstep(float low, float high, float v) {
+	return clamp((v - low) / (high - low), 0.0, 1.0);
+}
+
 float sample_variance_shadow(sampler2D shadow_map, vec2 texcoord, float compare) {
 	vec2 moments = texture2D(shadow_map, texcoord).xy;
 
 	float p = step(compare, moments.x);
-	float variance = max(moments.y - moments.x * moments.x, 0.00002);
+	float variance = max(moments.y - moments.x * moments.x, 0.000002);
 
 	float d = compare - moments.x;
-	float p_max = variance / (variance - d * d);
+	float p_max = linstep(0.2, 1.0, variance / (variance - d * d));
 
 	return min(max(p, p_max), 1.0);
 }
