c86c0c331424f3f9cbfd76974a5eafce9b362af9
Georg Schäfer <georg.peter.schaefer@gmail.com>
2015-07-11 16:10:29 +0200

    Register test actions for play state and connect them to the input.

--- src/source/zombye/gameplay/states/play_state.cpp
+++ src/source/zombye/gameplay/states/play_state.cpp
@@ -23,9 +23,10 @@ zombye::play_state::play_state(zombye::state_machine *sm) : sm_(sm) {
     auto input = sm->get_game()->input();
     input_ = input->create_manager();
 
-    input_->register_action("FIRE", []() {
+    /*input_->register_action("FIRE", []() {
         zombye::log("Peng Peng!");
-    });
+    });*/
+    input_->register_actions(*sm->get_game(), "scripts/input/play_state.as");
 
     input_->register_action("FIRE_END", []() {
         zombye::log("No more peng :(");
@@ -43,9 +44,17 @@ zombye::play_state::play_state(zombye::state_machine *sm) : sm_(sm) {
 
     input_->register_event("FIRE", input->mouse()->left_button());
     input_->register_keyboard_event("FIRE", "space");
-
     input_->register_up_event("FIRE_END", input->mouse()->left_button());
     input_->register_keyboard_up_event("FIRE_END", "space");
+
+    input_->register_keyboard_event("move_forward_begin", "w");
+    input_->register_keyboard_up_event("move_forward_end", "w");
+    input_->register_keyboard_event("move_backward_begin", "s");
+    input_->register_keyboard_up_event("move_backward_end", "s");
+    input_->register_keyboard_event("move_right_begin", "d");
+    input_->register_keyboard_up_event("move_right_end", "d");
+    input_->register_keyboard_event("move_left_begin", "a");
+    input_->register_keyboard_up_event("move_left_end", "a");
 }
 
 void zombye::play_state::enter() {
