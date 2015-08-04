5228b0f433cec7112aa0af34e2a8a9808fe0a502
Georg Schäfer <georg.peter.schaefer@gmail.com>
2015-08-04 19:14:10 +0200

    add refrence to rendering_system to mesh_manager

--- src/include/zombye/rendering/mesh_manager.hpp
+++ src/include/zombye/rendering/mesh_manager.hpp
@@ -8,6 +8,7 @@
 namespace zombye {
     class game;
     class mesh;
+    class rendering_system;
 }
 
 namespace zombye {
@@ -17,8 +18,9 @@ namespace zombye {
         friend class cached_resource_manager<const mesh, mesh_manager>;
 
         game& game_;
+        rendering_system& rendering_system_;
     public:
-        mesh_manager(game& game) noexcept;
+        mesh_manager(game& game, rendering_system& rendering_system) noexcept;
         ~mesh_manager() noexcept = default;
     protected:
         mesh_ptr load_new(const std::string& name);
--- src/source/zombye/rendering/mesh_manager.cpp
+++ src/source/zombye/rendering/mesh_manager.cpp
@@ -3,16 +3,17 @@
 #include <zombye/core/game.hpp>
 #include <zombye/rendering/mesh.hpp>
 #include <zombye/rendering/mesh_manager.hpp>
+#include <zombye/rendering/rendering_system.hpp>
 
 namespace zombye {
-    mesh_manager::mesh_manager(game& game) noexcept
-    : game_{game} { }
+    mesh_manager::mesh_manager(game& game, rendering_system& rendering_system) noexcept
+    : game_{game}, rendering_system_{rendering_system} { }
 
     mesh_ptr mesh_manager::load_new(const std::string& name) {
         auto asset = game_.asset_manager().load(name);
         if (!asset) {
             return nullptr;
         }
-        return std::make_shared<const mesh>(game_.rendering_system(), asset->content(), name);
+        return std::make_shared<const mesh>(rendering_system_, asset->content(), name);
     }
 }
